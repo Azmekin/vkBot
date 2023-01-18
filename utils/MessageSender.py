d267e5398c6b79d34b59b87fde20d12db2507060
sds <btv48817@nezid.com>
2023-01-18 17:39:05 +0700

    Delete token

--- utils/MessageSender.py
+++ utils/MessageSender.py
@@ -17,7 +17,7 @@ class MessageSender:
     def test_send_mess(message_info):
 
         a = {"user_id": "137413706", "message": message_info["message_text"], "random_id": str(int(random()*100)),
-             "access_token": "vk1.a.URLQjKYG7GoTX7xmKES9RyXPrNU8CcDknfKNulIvvPdv7cIWU0IhyiiEczl_scMXek8o_uKOVP0Uo_D18fqvzLUQqrbUpgMHDDf91-9DvUsDRdorqZYJd5_q1paV72FQ1YyIiu-nLJ4FUbGNE5fNslvgzcQFk8SeRx0tUtta2w6NkLit5baCHWegwusZQsOMF_OqphS0g5yteVSOndPuVQ",
+             "access_token": hiden_params.token,
              "v": "5.131"
              }
         r = requests.post("https://api.vk.com/method/"

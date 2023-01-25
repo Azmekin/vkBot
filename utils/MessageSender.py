c5acdf55cdd72e2cebe0e7245efaa044e8f89135
sds <btv48817@nezid.com>
2023-01-25 16:55:04 +0700

    Delete token twice

--- utils/MessageSender.py
+++ utils/MessageSender.py
@@ -8,7 +8,7 @@ import hiden_params
 class MessageSender:
     def send_message(message_info):
         a = {"user_id": message_info["user_id"], "message": message_info["message_text"], "random_id": str(int(random() * 100)),
-             "access_token": "vk1.a.URLQjKYG7GoTX7xmKES9RyXPrNU8CcDknfKNulIvvPdv7cIWU0IhyiiEczl_scMXek8o_uKOVP0Uo_D18fqvzLUQqrbUpgMHDDf91-9DvUsDRdorqZYJd5_q1paV72FQ1YyIiu-nLJ4FUbGNE5fNslvgzcQFk8SeRx0tUtta2w6NkLit5baCHWegwusZQsOMF_OqphS0g5yteVSOndPuVQ",
+             "access_token": hiden_params.token,
              "v": "5.131"
              }
         r = requests.post("https://api.vk.com/method/"
